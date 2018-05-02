1d8ab012757b2c3d34c19187f16bc5fea9cf488d
Josh Singer <josh.singer@glasgow.ac.uk>
2018-05-02 15:44:06 +0100

    initial stuff on clinical trials.

---
+++ glue/phdrLoadClinicalTrials.js
@@ -0,0 +1,13 @@
+var ctObjs;
+
+glue.inMode("module/phdrTabularUtility", function() {
+	ctObjs = glue.tableToObjects(glue.command(["load-tabular", "tabular/formatted/phdr_clinical_trials.txt"]));
+});
+
+_.each(ctObjs, function(ctObj) {
+	glue.command(["create", "custom-table-row", "phdr_clinical_trial", ctObj.id]);
+	glue.inMode("custom-table-row/phdr_clinical_trial/"+ctObj.id, function() {
+		glue.command(["set", "field", "display_name", ctObj.display_name]);
+		glue.command(["set", "field", "regimen", ctObj.regimen]);
+	});
+});
