00f2c32a1eb0bcaebc929dfc5d84880fb9017bd2
Josh Singer <josh.singer@glasgow.ac.uk>
2019-07-02 15:25:19 +0100

    simple population of phdr_alignment_drug -- resistance literature is "good" for all genotypes, 10 common subtypes, and otherwise "poor"

--- glue/phdrPopulateAlignmentDrugResistanceLiterature.js
+++ glue/phdrPopulateAlignmentDrugResistanceLiterature.js
@@ -1,4 +1,40 @@
-// this script is a placeholder for populating which drugs / subtypes have been properly tested.
+glue.command(["multi-delete", "phdr_alignment_drug", "-a"]);
+
+// method 1
+// All genotypes have good resistance literature for all drugs.
+// All subtypes have poor resistance literature for all drugs unless they are one of a list of common subtypes.
+
+var almtObjs = glue.tableToObjects(glue.command(["list", "alignment", "-w", "parent.name = 'AL_MASTER' or parent.parent+.name = 'AL_MASTER'"]));
+
+var drugIds = glue.getTableColumn(glue.command(["list", "custom-table-row", "phdr_drug"]), "id");
+
+var commonSubtypes = ["AL_1a", "AL_1b", "AL_2a", "AL_2b", "AL_2c", "AL_3a", "AL_3b", "AL_4a", "AL_4d", "AL_6a"];
+
+_.each(almtObjs, function(almtObj) {
+	var almtName = almtObj.name;
+	var almtParentName = almtObj["parent.name"];
+	
+	var resistanceLiterature = "poor";
+	
+	if(almtParentName == "AL_MASTER" || commonSubtypes.indexOf(almtName) >= 0) {
+		resistanceLiterature = "good";
+	} 
+	
+	_.each(drugIds, function(drugId) {
+		almtDrugId = almtName+":"+drugId;
+		glue.command(["create", "custom-table-row", "phdr_alignment_drug", almtDrugId]);
+		glue.inMode("/custom-table-row/phdr_alignment_drug/"+almtDrugId, function() {
+			glue.command(["set", "link-target", "alignment", "alignment/"+almtName]);
+			glue.command(["set", "link-target", "phdr_drug", "custom-table-row/phdr_drug/"+drugId]);
+			glue.command(["set", "field", "resistance_literature", resistanceLiterature]);
+		});
+	});
+});
+
+/*
+// method 2
+
+// this alternative script is another way to populate which drugs / subtypes have good evidence.
 // it's based on a simple scan of the resistance findings. 
 // however this should really be done using a curated table: this placeholder method
 // misclassifies drug / subtype combinations where there is genuinely no 
@@ -62,3 +98,6 @@ _.each(almtObjs, function(almtObj) {
 		});
 	});
 });
+
+
+*/
